bc6c223cd04524c008da61d69cc12dcd22bb3c07
Eneakao <info@untitled.es>
2021-05-17 14:50:16 +0200

    terminado el paginado manual hacia delante, recorre todas las descripciones

--- src/nfts.svelte
+++ src/nfts.svelte
@@ -1,7 +1,4 @@
 <script>
-import { dataset_dev } from "svelte/internal";
-
-
 	let valorWallet = ' '
 	let claseGuardarLocalstorage = ''
 	const onFocus = () => valorWallet = '';
@@ -13,10 +10,10 @@ import { dataset_dev } from "svelte/internal";
 	let arrayDatosTimeLine = []
 	let arrayIdsTimeLine = []
 	let objetoTimeLine = {}
+	let whale = ''
 
 	let offsetTokens = 0
 	let limitTokens = 20
-	let numTokenList = 0
 	let limitDisabled = false
 	let offsetDisabled = false
 
@@ -94,27 +91,6 @@ import { dataset_dev } from "svelte/internal";
 		}
 	}
 
-	$: {
-		if ((numTokenList != '') && (numTokenList !=0)){
-			limitTokens = numTokenList
-			listadosTimeLine()
-		}
-
-	}
-
-	const sumaOffset = () => {
-		offsetTokens = parseInt(offsetTokens) + parseInt(limitTokens)
-		listadosTimeLine()
-	}
-	const restaOffset = () => {
-		if (parseInt(offsetTokens) - parseInt(limitTokens)<0){
-			offsetTokens = 0
-		} else {
-			offsetTokens = parseInt(offsetTokens) - parseInt(limitTokens)
-		}
-		listadosTimeLine()
-	}
-
 	const listados = async() => {
 		arrayDatos = []
 		try {
@@ -182,23 +158,42 @@ import { dataset_dev } from "svelte/internal";
 		return str;
 	}
 
+	const sumaOffset = () => {
+		offsetTokens = parseInt(offsetTokens) + parseInt(limitTokens)
+		listadosTimeLine()
+	}
+	const inicioOffset = () => {
+		offsetTokens = 0
+		listadosTimeLine()
+	}
+
 	const listadosTimeLine = async() => {
 		limitDisabled = true
 		offsetDisabled = true
         arrayDatosTimeLine = []
         arrayIdsTimeLine = []
         try {
-			const res = await fetch(`https://api.ergoplatform.com/api/v1/tokens?offset=${offsetTokens}&limit=${limitTokens}`)
-            const data = await res.json()
 
-			const arrayIdsTimeLine = data.items.filter(token => token.emissionAmount <= 1)
-				
+			const res = await fetch(`https://api.ergoplatform.com/api/v1/tokens?offset=${offsetTokens}&limit=${limitTokens}`)
+			const data = await res.json()
+			arrayIdsTimeLine = data.items.filter(token => token.emissionAmount <= 1)
+			if (arrayIdsTimeLine.length < (limitTokens - 1)) {
+				whale = 'If you see that it takes a while, wait a few seconds, be patient. You are going through the thousands of tokens created in the time of the whale...'
+				do {
+					offsetTokens = parseInt(offsetTokens) + parseInt(limitTokens)
+					const res = await fetch(`https://api.ergoplatform.com/api/v1/tokens?offset=${offsetTokens}&limit=${limitTokens}`)
+					const data = await res.json()
+					arrayIdsTimeLine = arrayIdsTimeLine.concat(data.items.filter(token => token.emissionAmount <= 1))
+				} while (arrayIdsTimeLine.length < limitTokens)
+			}
+			whale = ''
+
             for(let i = 0; i < arrayIdsTimeLine.length; i++){
                 const res2 = await fetch(`https://api.ergoplatform.com/api/v0/assets/${arrayIdsTimeLine[i].id}/issuingBox`)
                 const data2 = await res2.json()
-
+				
 				let R7info = data2.map(token => token.additionalRegisters.R7)
-				if (R7info == '0e020101' || R7info == '0e020102') {
+				if (R7info == '0e020101' || R7info == '0e020102'){
 					let R9info = data2.map(token => token.additionalRegisters.R9)
 					let R5info = data2.map(token => token.additionalRegisters.R5)
 					if (R9info != '') {
@@ -211,8 +206,7 @@ import { dataset_dev } from "svelte/internal";
 							ext: data2.map(token => toUtf8String(token.additionalRegisters.R9).substr(2).slice(-4)),
 							urlInTxt: urlDescription
 						}
-					} else if (R9info == '') {
-						if (arrayIdsTimeLine[i].description != '') {
+					} else if ((R9info == '') && (arrayIdsTimeLine[i].description != '')) {
 							linkify(arrayIdsTimeLine[i].description)
 							objetoTimeLine = {
 								id: data2.map(token => token.assets[0].tokenId),
@@ -222,12 +216,11 @@ import { dataset_dev } from "svelte/internal";
 								ext: urlDescription.slice(-4),
 								urlInTxt: urlDescription
 							}
-						}
 					}
 				}
 				arrayDatosTimeLine[i] = objetoTimeLine
-				objetoTimeLine = {id: '', name: '', description: '', r9: '', ext: '' }
-
+				urlDescription = ''
+				objetoTimeLine = {id: '', name: '', description: '', r9: '', ext: '', urlInTxt: '' }
 			}
 			for(let j = 0; j < arrayDatosTimeLine.length; j++){
                 for (let k = 0; k < arrayFavorites.length; k++){
@@ -238,13 +231,12 @@ import { dataset_dev } from "svelte/internal";
             }
 			limitDisabled = false
 			offsetDisabled = false
-			console.log(arrayIdsTimeLine)
         } catch (error) {
             console.log(error)
         }
 	}
 	
-var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+var urlRegex =/(\b(https?|ftp|file|ipfs):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 function linkify(text) {
     return text.replace(urlRegex, function(url) {
 		urlDescription = url
@@ -262,19 +254,16 @@ listadosTimeLine()
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
 </svelte:head>
-
-
 
 <!-- Cabecera -->
 <main class=" bg-dark">
 	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 		<a href="https://ergonfts.org" class="mb-1 mx-3 separaI"><img src="ergo.png" alt="Logotype Ergo" width="100"></a>
-
 		<button class="navbar-toggler mx-3" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-			
 		<div class="collapse navbar-collapse interiorT" id="navbar">
 			<a href="https://ergotokens.org" title="Ergo Tokens" class="anchoT btn bg-dark text-secondary border border-secondary mx-2">Tokens</a>
 			<div class="input-group bg-dark">
@@ -283,9 +272,7 @@ listadosTimeLine()
 				<input on:focus={onFocus} bind:value={valorWallet} class="form-control border border-dark" placeholder="Your wallet"  aria-label="Your wallet" aria-describedby="basic-addon1">
 				<button on:click={guardarLocalstorage} title="Add wallet" class="input-group-text dropdown border border-dark bg-light {claseGuardarLocalstorage}" id="basic-addon1"><i class="bi bi-wallet-fill"></i></button>
 			</div>
-			
 			<button on:click={listados} class="anchoT btn border-secondary border-seconda text-light mx-2 my-2 " title="Accept">Accept</button>
-
 			<!-- Wallet -->
 			<select bind:value={selected} class="nav-link bg-dark border border-secondary text-light mx-2 rounded">
 				<option value=0 class="dropdown-item" selected>Your Wallets</option>
@@ -364,25 +351,20 @@ listadosTimeLine()
 	<div class="my-2 bg-light pb-1">
 		<div class="bg-secondary py-3 px-5 text-light border-bottom border-dark">
 			
-				<i class="bi bi-wind"></i>
-				<span>The last NFTs</span>
-				
-				<!-- <select bind:value={numTokenList} disabled={limitDisabled} class="nav-link bg-secondary border border-dark text-light mx-2 rounded">
-					<option value=0>Select</option>
-					<option value=10>10</option>
-					<option value=20>20</option>
-					<option value=50>50</option>
-				</select>
-				<button on:click={restaOffset} disabled={offsetDisabled}>-</button>
-				<button on:click={sumaOffset} disabled={offsetDisabled}>+</button> -->
-				
+				<div class="row justify-content-start"> 
+				  <div class="col-2">
+					<i class="bi bi-wind"></i>
+					<span>The last NFTs</span>
+				  </div>
+				</div>
+			
 		</div>
 		<div class="row mx-2 my-2">
 			{#await listadosTimeLine}
 				<p class="text-secondary">Loading...</p>
 			{:then listadosTimeLine}
 				{#each arrayDatosTimeLine as datos}
-					{#if datos.ext == '.png' || datos.ext == '.gif' || datos.ext == '.jpg' || datos.ext == 'jpeg' || datos.ext == '.bmp' || datos.ext == '.svg' || datos.ext == '.raf' || datos.ext == '.nef'}
+					{#if datos.ext == 'ink/' || datos.ext == '.png' || datos.ext == '.gif' || datos.ext == '.jpg' || datos.ext == 'jpeg' || datos.ext == '.bmp' || datos.ext == '.svg' || datos.ext == '.raf' || datos.ext == '.nef'}
 						<div class="card mt-2 mx-1 cardColor">
 							<div>
 								{#if datos.class == true}
@@ -394,8 +376,12 @@ listadosTimeLine()
 							<a href={datos.r9} target="_blank" title={datos.name}>
 								<img src={datos.r9} class="card-img-top mb-3 imageBorder" alt={datos.name} width="200">
 							</a>
-							<span class="h4">{datos.name}</span><span class="small mb-2">{datos.description}</span>
-							<a href={datos.urlInTxt} class="mb-2">{datos.urlInTxt.substring(8, 30)}...</a>
+							<span class="h6">{datos.name}</span><span class="small mb-2 text-secondary">{datos.description}</span>
+							{#if (datos.urlInTxt !='')}
+								<hr class="text-secondary">
+								<span class="mb-3 text-dark"><i class="bi bi-link"></i>
+								<a href={datos.urlInTxt} class="text-dark small">{datos.urlInTxt.substring(8, 30)}...</a></span>
+							{/if}
 						</div>
 					{/if}
                     {#if datos.ext == '.mp3' || datos.ext == '.ogg' || datos.ext == '.wma' || datos.ext == '.wav' || datos.ext == '.aac' || datos.ext == 'aiff'}
@@ -409,8 +395,13 @@ listadosTimeLine()
                                 {datos.name}
 							</div>
 								<audio src={datos.r9} class="card-img-top mb-3 " title={datos.name} controls></audio> 
-								<span class="h6">{datos.name}</span><span class="small mb-2">{datos.description}</span>
-								<a href={datos.urlInTxt} class="mb-2">{datos.urlInTxt.substring(8, 30)}...</a>
+								<span class="h6">{datos.name}</span><span class="small mb-2 text-secondary">{datos.description}</span>
+								{#if (datos.urlInTxt !='')}
+									<hr class="text-secondary">
+									<span class="mb-3 text-dark"><i class="bi bi-link"></i>
+									<a href={datos.urlInTxt} class="text-dark small">{datos.urlInTxt.substring(8, 30)}...</a>
+									</span>
+								{/if}
 						</div>
 					{/if}
 				{/each}
@@ -418,7 +409,11 @@ listadosTimeLine()
 				<p>Error</p>
 			{/await}
 		</div>
-	</div> 
+		<div class="bg-secondary py-3 px-5 text-light border-bottom border-dark">
+			<button on:click={sumaOffset} disabled={offsetDisabled} class="bg-secondary text-light border border-secondary"><i class="bi bi-arrow-right-square-fill" style="font-size: 2rem;"></i></button>
+			<span class="small">{whale}</span>
+		</div>
+	</div>
 </main>
 
 <style>
@@ -432,28 +427,20 @@ listadosTimeLine()
 	.imageBorder{
 		border: 5px solid #ffffff;
 	}
-
 	@media only screen and (max-width: 768px) {
 		.cardColor{
 			width: 97%;
 		}	
-		
 		.anchoT {
 			width: 97%;
 			margin-bottom: 10px;
 			margin-left: 0px;
-
 		}
-
 		.interiorT{
 			padding: 10px;
 		}
-
 		.separaI{
 			margin-top: 5px;;
 		}
-    
 	}
-	
-
 </style>
