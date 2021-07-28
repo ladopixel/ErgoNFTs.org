d30b987bb2dbc784cee073275608364f2c792d9d
Eneakao <info@untitled.es>
2021-07-28 16:37:31 +0200

    comiezo a leer metadatos de R5

--- src/nfts.svelte
+++ src/nfts.svelte
@@ -220,6 +220,7 @@
 							ext: data2.map(token => toUtf8String(token.additionalRegisters.R9).substr(2).slice(-4)),
 							urlInTxt: urlDescription
 						}
+						console.log(objetoTimeLine)
 					} else if ((R9info == '') && (arrayIdsTimeLine[i].description != '')) {
 							linkify(arrayIdsTimeLine[i].description)
 							objetoTimeLine = {
@@ -291,16 +292,6 @@ if (JSON.stringify($location).substr(2)){
 
 listadosTimeLine ()
 </script>
-
-<!-- <svelte:head>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="popper.min.js"></script>
-	<script src="bootstrap.js"></script>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
-</svelte:head> -->
 
 <!-- Cabecera -->
 <main class=" bg-dark">
